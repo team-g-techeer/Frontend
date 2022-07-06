770aa12ad5d4490b468addb7ad793ae4a5a35042
hee <jkgt0521@naver.com>
2022-07-07 00:51:53 +0900

    Design: #5#6 디자인 수정 및 모달 창 추가

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -4,11 +4,29 @@ import GlobalStyles from '../styles/GlobalStyle';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { Link } from 'react-router-dom';
+import '../styles/Calendar.css';
+import moment from 'moment';
 import styled from 'styled-components';
+
+const ProjectDiv = styled.div`
+    width: 90%;
+    background-color: #b9dcf8;
+    margin: 0 auto;
+    margin-top: 20px;
+    border-radius: 30px;
+    padding: 11px;
+`;
 
 
 function Main() {
-    const [value, onChange] = useState(new Date());
+    const [value, onChangeDate] = useState(new Date());    //초기값은 현재의 날짜
+
+    const marks = [
+        "18-07-2022",
+        "12-07-2022",
+        "22-07-2022"
+    ];
+
     return (
         <div>
             <GlobalStyles></GlobalStyles>
@@ -29,9 +47,12 @@ function Main() {
                     fontSize: '10px'
                 }}>로그아웃</Link>
             </div>
-                <CalendarContainer>
-                    <Calendar onChange={onChange} value={value}></Calendar>
-                </CalendarContainer>
+               <Calendar onChange={onChangeDate} value={value}
+               tileClassName={({date, view}) => {
+                if(marks.find(x => x === moment(date).format('DD-MM-YYYY'))) {
+                    return 'highlight'
+                }
+               }}></Calendar>
             </div>
             <div className='right'>
                 <img className='logoImage2' alt='logo' src='assets/imgs/logo_x.png' ></img>
@@ -42,6 +63,9 @@ function Main() {
                     </div>
                     <div className='projectList'>
                         Project
+                        <p className='addProject'><Link to="/signup">+프로젝트 추가하기</Link></p>
+                        <ProjectDiv><Link className='proTitle' to="/project">웹 개발 프로젝트</Link></ProjectDiv>
+                        <ProjectDiv><Link className='proTitle' to="/project">산책 동아리</Link></ProjectDiv>
                     </div>
                 </div>
             </div>
@@ -49,11 +73,5 @@ function Main() {
     );
 
 }
-
-const CalendarContainer = styled.div`
-    margin: auto;
-    background-color: #ddecf8;
-    font-family: GmarketSansMedium;   //폰트 적용 안됨 ㅠㅠ
-`
 
 export default Main;
